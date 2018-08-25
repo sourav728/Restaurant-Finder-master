248c069dbc28fe2e30a8fc2644affe915b33ffea
Sourav Nandi <souravnandi728@outlook.com>
2018-08-25 17:57:15 +0530

    Initial commit

---
+++ app/src/main/java/srv/restaurantfinder/controller/Splash_Screen.java
@@ -0,0 +1,33 @@
+package srv.restaurantfinder.controller;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import srv.restaurantfinder.R;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash__screen);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow(); // in Activity's onCreate() for instance
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash_Screen.this,MapsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+    }
+}
---
+++ app/src/main/java/srv/restaurantfinder/service/RetrofitAPI.java
@@ -0,0 +1,26 @@
+package srv.restaurantfinder.service;
+
+import srv.restaurantfinder.model.Result;
+import srv.restaurantfinder.model.ResultDetails;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Interface to include calls.
+ */
+public interface RetrofitAPI {
+
+    @GET("textsearch/json")
+    Call<Result> getResultFromTextSearch(@Query("query") String query,
+                                         @Query("type") String type,
+                                         @Query("key") String key,
+                                         @Query("radius") Integer radius,
+                                         @Query("maxprice") Integer maxPrice,
+                                         @Query("opennow") Boolean openNow);
+
+    @GET("details/json")
+    Call<ResultDetails> getRestaurantById(@Query("placeid") String placeId,
+                                          @Query("key") String key);
+
+}
